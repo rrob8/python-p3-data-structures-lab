ab30794316e715777eeee665c424429f86dc1ecb
Robert Bubnik <rbubnik@gmail.com>
2024-04-02 21:51:25 -0400

    passing all tests

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -25,19 +25,36 @@ def get_names(spicy_foods):
 def get_spiciest_foods(spicy_foods):
         spicy_item = []
         for item in spicy_foods:
-            spicy_item.append(item) 
+            if item['heat_level'] >5:
+                spicy_item.append(item)
+        return(spicy_item)
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for item in spicy_foods:
+         print(item['name'] + ' (' + item['cuisine']+ ') ' + "| " + 'Heat Level: ' + int(item['heat_level'])*"🌶")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for item in spicy_foods:
+         if item['cuisine'] == cuisine:
+              return item
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    for item in spicy_foods:
+         if item['heat_level'] > 5:
+            print(item['name'] + ' (' + item['cuisine']+ ') ' + "| " + 'Heat Level: ' + int(item['heat_level'])*"🌶")
 
 def get_average_heat_level(spicy_foods):
-    pass
+    sum = 0
+    count = 0
+    for item in spicy_foods:
+        sum += item['heat_level']
+        count +=  1
+    average = sum/count
+    return average
+
+
+
 
 def create_spicy_food(spicy_foods, spicy_food):
-    pass
+    spicy_foods.append(spicy_food)
+    return spicy_foods
